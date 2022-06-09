f0e2178726a40df30042f4452ec97c927c1f7ef1
Timhung803 <timhkt@gmail.com>
2022-06-10 01:57:01 +1200

    Updated Context API & useContext Hook with RR V6

--- src/Layout.js
+++ src/Layout.js
@@ -2,12 +2,9 @@ import Header from "./Header";
 import Nav from "./Nav";
 import Footer from "./Footer";
 import { Outlet } from 'react-router-dom';
-import { useContext } from 'react';
-import DataContext from './context/DataContext';
 
 
 const Layout = () => {
-  const { width } = useContext(DataContext);
   return (
     <div className="App">
       <Header title="React JS Blog" />
